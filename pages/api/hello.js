368e91adc0d519ac5db29c88d823b3c8030637e9
kouadjo ange <kouassi.kouadjo@smile.ci>
2023-04-21 00:45:07 +0000

    oktest

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,5 +1,9 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
 export default function handler(req, res) {
-  res.status(200).json({ name: 'John Doe' })
+    res.status(200).json({
+        nom: 'John Doe',
+        image: 'https://play-lh.googleusercontent.com/-Mp3XW7uhwn3KGQxUKGPoc4MbA5ti-3-q23TgoVi9ujBgHWW5n4IySvlG5Exwrxsjw=w600-h300-pc0xffffff-pd',
+        isActive: 1
+    })
 }
